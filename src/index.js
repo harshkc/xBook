c5f43972dbcced4828ad7964c22bd091e11628d8
harshkc <harsh.hkc1006@gmail.com>
2021-10-26 16:53:25 +0530

    :art: UI: cleans old modal code and uses reusable modal component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+/** @jsx jsx */
+import {jsx} from '@emotion/core'
+
+import 'bootstrap/dist/css/bootstrap-reboot.css'
 import '@reach/dialog/styles.css'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import {Dialog} from '@reach/dialog'
+import {Button, Input, FormGroup, Spinner} from './components/lib'
+import {Modal, ModalContents, ModalOpenButton} from './components/modal'
 import {Logo} from './components/logo'
 
-function LoginForm({onSubmit, buttonText}) {
+function LoginForm({onSubmit, submitButton}) {
   function handleSubmit(event) {
     event.preventDefault()
     const {username, password} = event.target.elements
@@ -16,25 +21,36 @@ function LoginForm({onSubmit, buttonText}) {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
+    <form
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'stretch',
+        '> div': {
+          margin: '10px auto',
+          width: '100%',
+          maxWidth: '300px',
+        },
+      }}
+      onSubmit={handleSubmit}
+    >
+      <FormGroup>
         <label htmlFor="username">Username</label>
-        <input id="username" />
-      </div>
-      <div>
+        <Input id="username" />
+      </FormGroup>
+      <FormGroup>
         <label htmlFor="password">Password</label>
-        <input id="password" type="password" />
-      </div>
+        <Input id="password" type="password" />
+      </FormGroup>
       <div>
-        <button type="submit">{buttonText}</button>
+        {React.cloneElement(submitButton, {type: 'submit'})}
+        <Spinner css={{marginLeft: 5}} />
       </div>
     </form>
   )
 }
 
 function App() {
-  const [openModal, setOpenModal] = React.useState('none')
-
   function login(formData) {
     console.log('login', formData)
   }
@@ -44,29 +60,48 @@ function App() {
   }
 
   return (
-    <div>
+    <div
+      css={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: '100%',
+        height: '100vh',
+      }}
+    >
       <Logo width="80" height="80" />
       <h1>Bookshelf</h1>
-      <div>
-        <button onClick={() => setOpenModal('login')}>Login</button>
+      <div
+        css={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(2, minmax(0, 1fr))',
+          gridGap: '0.75rem',
+        }}
+      >
+        <Modal>
+          <ModalOpenButton>
+            <Button variant="primary">Login</Button>
+          </ModalOpenButton>
+          <ModalContents aria-label="Login form" title="Login">
+            <LoginForm
+              onSubmit={login}
+              submitButton={<Button variant="primary">Login</Button>}
+            />
+          </ModalContents>
+        </Modal>
+        <Modal>
+          <ModalOpenButton>
+            <Button variant="secondary">Register</Button>
+          </ModalOpenButton>
+          <ModalContents aria-label="Registration form" title="Register">
+            <LoginForm
+              onSubmit={register}
+              submitButton={<Button variant="secondary">Register</Button>}
+            />
+          </ModalContents>
+        </Modal>
       </div>
-      <div>
-        <button onClick={() => setOpenModal('register')}>Register</button>
-      </div>
-      <Dialog aria-label="Login form" isOpen={openModal === 'login'}>
-        <div>
-          <button onClick={() => setOpenModal('none')}>Close</button>
-        </div>
-        <h3>Login</h3>
-        <LoginForm onSubmit={login} buttonText="Login" />
-      </Dialog>
-      <Dialog aria-label="Registration form" isOpen={openModal === 'register'}>
-        <div>
-          <button onClick={() => setOpenModal('none')}>Close</button>
-        </div>
-        <h3>Register</h3>
-        <LoginForm onSubmit={register} buttonText="Register" />
-      </Dialog>
     </div>
   )
 }
